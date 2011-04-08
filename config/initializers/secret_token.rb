9162e6765dcde5dc958d651313eb379a5e98b82a
unknown <Paul@.(none)>
2011-04-07 21:15:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectIronMan::Application.config.secret_token = '3bd41bf45a4901fb8893bec51697696f26bb0350dad759adef2116c1cccce8d3175f60db4dc71be9434de800e4f278bcbf2c8a228b7bcc3167c991d5e33b7a23'
